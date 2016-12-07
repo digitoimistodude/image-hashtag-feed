8f9ca4141505740538f1c51e2408705eab2279a2
Timi Wahalahti <timi.wahalahti@gmail.com>
2016-12-07 11:42:19 +0200

    Fix broken feed due to Instagam update

--- includes/fetch-instagram.php
+++ includes/fetch-instagram.php
@@ -25,9 +25,26 @@ Class Dude_Img_Hashfeed_Fetch_Instagram extends Dude_Img_Hashfeed {
 
 		$parameters = apply_filters( 'dude_img_hashfeed_insta_fetch_parameters', "ig_hashtag($hashtag) { media.first($count) { count, nodes { caption, code, comments { count }, date, display_src, id, is_video, likes { count }, owner { id, username, full_name, profile_pic_url }, thumbnail_src, video_views, video_url }, page_info } }" );
 		$parameters = urlencode( $parameters );
-    $url = "https://www.instagram.com/query/?q=$parameters&ref=tags%3A%3Ashow";
-    $insta = json_decode( file_get_contents( $url ) );
-    $insta = $insta->media->nodes;
+    if( !function_exists( 'curl_init' ) ) {
+      return false;
+    }
+
+    $random = self::generate_random_string();
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, "https://www.instagram.com/query/");
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, 'q='.$parameters);
+    $headers = array();
+    $headers[] = "Cookie:  csrftoken=$random;";
+    $headers[] = "X-Csrftoken: $random";
+    $headers[] = "Referer: https://www.instagram.com/";
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    $output = curl_exec($ch);
+    curl_close($ch);
+
+    $output = json_decode( $output );
+    $insta = $output->media->nodes;
 
 		if( empty( $insta ) )
 			return false;
@@ -43,4 +60,16 @@ Class Dude_Img_Hashfeed_Fetch_Instagram extends Dude_Img_Hashfeed {
 
 		return $return;
 	} // end function do_fetch
+
+  protected static function generate_random_string( $length = 10 ) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+
+    for( $i = 0; $i < $length; $i++ ) {
+      $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+
+    return $randomString;
+ } // end function generate_random_string
 } // end class Dude_Img_Hashfeed_Admin_Settings_Page
